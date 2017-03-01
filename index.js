3b4920e12e5030d79dbdb3f3b4b05282c08f14a0
Ammar <ammar.mian@gmail.com>
2017-03-01 11:32:13 -0500

    basic hello world

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const app = require('./lib');
+const port = process.env.PORT || 4873;
+
+
+// Start the server
+
+app.listen(port, () => {
+  if (process.env.DEV) {
+    console.info('[ MCI-REGISTRY ] HTTP server listening on port ' + port);
+    console.info(`[ MCI-REGISTRY ] Server running with node version: ${process.version} `);
+  }
+
+  process.emit('ready');
+});
